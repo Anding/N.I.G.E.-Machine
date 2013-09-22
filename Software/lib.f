1de0f97a95e3331082450fc1fe55d234874c4158
Andrew Read <anding_eunding@yahoo.com>
2013-09-22 18:54:06 +0800

    Removed redundant files

--- Software/lib.f
+++
@@ -1,8 +0,0 @@
-\ STRUCTURE words, machine code optimized, following http://www.forth200x.org/structures.html
-: begin-structure : here 1+ 0 55 c, 0 , postpone ; ;	\ 55 is the opcode for #.l which will be compiled into the definition
-: end-structure swap ! ;
-: +field over : postpone literal 15 c, postpone ; + ;	\ 15 is the opcode for +
-: field: 4 +field ;
-: wfield: 2 +field ;
-: cfield: 1 +field ;
-
