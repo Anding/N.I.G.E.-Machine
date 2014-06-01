810ed88e693143dbc1293368899daa59e0bafa3e
Andrew Read <anding_eunding@yahoo.com>
2014-06-01 12:29:31 +0800

    Implemented local variables Move BASE and in/out vectors to exception stack Updated N.I.G.E. Machine manual

--- System/TestCases/test.asm
+++ System/TestCases/test.asm
@@ -1,12 +1,29 @@
-	nop
-	nop
-	nop
-	nop
-	#.b	1
-	#.b	2
-	#.b	3
-	+
-	+
-	#.b	6
-	-
-l0	bra l0
+local0		equ	hex 03F000
+local1		equ	hex 03F004
+sevenseg	equ	hex 03F830
+		nop
+		nop
+		nop
+		nop
+		#.b	1
+		#.b	2
+		jsl	test
+here		bra	here
+;
+;
+double		#.l	local0
+		store.l
+		#.l	local0
+		fetch.l
+		2*
+		rts
+test		#.l	local1
+		store.l
+		#.l	local0
+		store.l
+		#.l	local1
+		fetch.l
+		jsl	double
+		#.l	local0
+		fetch.l
+		rts
